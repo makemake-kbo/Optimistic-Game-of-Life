cba883b76ed401fb4b02f265120a400c9b09c170
Vukasin Gostovic <vukasin@gostovic.me>
2022-03-16 14:04:43 +0100

    Update contract address and ABI for optimism mainnet deployment

--- scripts/contractinfo.js
+++ scripts/contractinfo.js
@@ -1,53 +1,54 @@
-const contractAddress = '0x1D54216004D4cf75fbb335E9D5089D967742cec5';
+const contractAddress = '0x5A00D05bA499d5113959dF92f8F6ca62C0269F3D';
 
 const contractABI = [
-{
-	"inputs": [],
-	"stateMutability": "nonpayable",
-	"type": "constructor"
-},
-{
-	"anonymous": false,
-	"inputs": [],
-	"name": "boardReset",
-	"type": "event"
-},
-{
-	"anonymous": false,
-	"inputs": [],
-	"name": "evaluated",
-	"type": "event"
-},
-{
-	"inputs": [],
-	"name": "evaluate",
-	"outputs": [],
-	"stateMutability": "nonpayable",
-	"type": "function"
-},
-{
-	"inputs": [],
-	"name": "reset",
-	"outputs": [],
-	"stateMutability": "nonpayable",
-	"type": "function"
-},
-{
-	"inputs": [
 	{
-		"internalType": "uint8",
-		"name": "i",
-		"type": "uint8"
-	}
-	],
-	"name": "viewRow",
-	"outputs": [
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
 	{
-		"internalType": "uint8[7]",
-		"name": "",
-		"type": "uint8[7]"
+		"anonymous": false,
+		"inputs": [],
+		"name": "boardReset",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "evaluate",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [],
+		"name": "evaluated",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "reset",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint8",
+				"name": "i",
+				"type": "uint8"
+			}
+		],
+		"name": "viewRow",
+		"outputs": [
+			{
+				"internalType": "uint8[7]",
+				"name": "",
+				"type": "uint8[7]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
 	}
-	],
-	"stateMutability": "view",
-	"type": "function"
-}];
+];
